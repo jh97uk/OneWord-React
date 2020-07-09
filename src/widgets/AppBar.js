220417bd18cb8a4bf703b34f071e2b283e57dc0a
ItsBloo <jamesheald@protonmail.com>
2020-07-09 21:01:18 +0100

    Added backLink function functionality

--- src/widgets/AppBar.js
+++ src/widgets/AppBar.js
@@ -6,13 +6,23 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBackward, faLongArrowAltLeft } from '@fortawesome/free-solid-svg-icons';
 import {Link, BrowserRouter as Router} from 'react-router-dom';
 function AppBar(props) {
+    var backLink = (<></>)
+    if(props.onBackButtonFunction){
+        backLink = (
+            <Link onClick={props.onBackButtonFunction}>
+             <FontAwesomeIcon icon={faLongArrowAltLeft}/>
+            </Link>)
+    } else{
+        backLink = (
+            <Link to={props.backRoute}>
+             <FontAwesomeIcon icon={faLongArrowAltLeft}/>
+            </Link>)
+    }
     
     return (
     <div className="appBar">
         <div className="buttonsLeft">
-            <Link to={props.backRoute}>
-                <FontAwesomeIcon icon={faLongArrowAltLeft}/>
-            </Link>
+            {backLink}
         </div>
         <h1>{props.pageTitle}</h1>
         <div className="buttonsRight">
