fea0bb4332db96dd8130b96dce2adbec271ad228
James <jh97uk@icloud.com>
2020-10-30 17:51:26 +0000

    Fixed back button not working

--- src/widgets/AppBar.js
+++ src/widgets/AppBar.js
@@ -5,22 +5,24 @@ import 'font-awesome/css/font-awesome.min.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBackward, faLongArrowAltLeft } from '@fortawesome/free-solid-svg-icons';
 import {Link, BrowserRouter as Router} from 'react-router-dom';
-import {IconButton} from '@material-ui/core';
+import {Button, IconButton} from '@material-ui/core';
 
 function AppBar(props) {
     const buttonStyle = {padding:0, marginLeft:15, color:'white'};
+    console.log(props);
     return (
     <div className="appBar">
         <div className="buttonsLeft">
             {
                 props.onBackButtonFunction ? 
-                    <IconButton style={buttonStyle} component={(props)=><Link to={props.backRoute}/>}>
-                        <FontAwesomeIcon icon={faLongArrowAltLeft}/>
-                    </IconButton>
-                    :
                     <IconButton style={buttonStyle} onClick={props.onBackButtonFunction}>
                         <FontAwesomeIcon icon={faLongArrowAltLeft}/>
                     </IconButton>
+                    :
+                    <IconButton style={buttonStyle} component={(compProps)=><Link {...compProps} to={props.backRoute}/>}>
+                        <FontAwesomeIcon icon={faLongArrowAltLeft}/>
+                    </IconButton>
+                    
             }
         </div>
         <h1>{props.pageTitle}</h1>
